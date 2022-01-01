2968935550b244149abd989abfccf226802d01d9
weissye <weissye@post.bgu.ac.il>
2022-01-02 00:24:07 +0200

    Run model or mapper

--- src/main/java/il/ac/bgu/cs/bp/bpjs/bereshit/Main.java
+++ src/main/java/il/ac/bgu/cs/bp/bpjs/bereshit/Main.java
@@ -1,5 +1,6 @@
 package il.ac.bgu.cs.bp.bpjs.bereshit;
 
+import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
@@ -25,13 +26,7 @@ public class Main {
    */
 
   private static final Example example =
-//  Example.abpStudents;
-//          Example.abp;
     Example.Bereshit;
-  //  Example.HotCold;
-  //  Example.SampleProgram;
-//      Example.SampleProgram;
-    //  Example.TicTacToe;
 
   /**
    * internal context events are: "CTX.Changed", "_____CTX_LOCK_____", "_____CTX_RELEASE_____"
@@ -40,68 +35,60 @@ public class Main {
    * Level.NONE : print none
    * Level.CtxChanged: print only CTX.Changed events (i.e., filter the transaction lock/release events)
    */
-  //private static final Level logLevel = Level.CtxChanged;
-  // private static final Level logLevel = Level.ALL;
   private static final Level logLevel = Level.ALL;
-    // private static Queue<TestElement> q = new LinkedList<>();
 
   
   public static void main(final String[] args) throws Exception {
 
-//      Set<List<BEvent>> samples = new HashSet<>();
-//
-//      List<String> files =
-//          Arrays.stream(Objects.requireNonNull(Path.of(Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource(example.name())).toURI()).toFile().listFiles()))
-//              .map(f -> String.join("/", example.name(), f.getName()))
-//              .collect(Collectors.toList());
-//      System.out.println("files-"+files);
-//      BProgram bprog = new ContextBProgram(files);
-//      final BProgramRunner rnr = new BProgramRunner(bprog);
-//      rnr.addListener(new PrintCOBProgramRunnerListener(logLevel, new PrintBProgramRunnerListener()));
-//
-//      if (example == Example.TicTacToe) {
-////        boolean useUI = true;
-////        TicTacToeGameMain.main(bprog, rnr, useUI);
-//        return;
-//      }
-//      var eventLogger = rnr.addListener(new InMemoryEventLoggingListener());
-//      rnr.run();
-//      samples.add(eventLogger.getEvents());
+      System.out.println("// start");
 
+      // run runBPModel() function for Bereshit BP model or runMapper() function for model mapper (Bereshit.dot)
+      runBPModel();
+//      runMapper();
 
-       System.out.println("// start");
-//      String name = "abp";
-      String name = "Bereshit";
-     // This will load the program file  <Project>/src/main/resources/HelloBPjsWorld.js
-       BProgram bprog = new ContextBProgram(name + "/dal.js", name + "/bl.js");
-
-       // You can use a different EventSelectionStrategy, for example:
-       /* var ess = new PrioritizedBSyncEventSelectionStrategy();
-       bprog.setEventSelectionStrategy(ess); */
-       StateSpaceMapper mpr = new StateSpaceMapper();
-       mpr.setMaxTraceLength(10);
-       var res = mpr.mapSpace(bprog);
-       System.out.println("// completed mapping the states graph");
-       System.out.println(res.toString());
-
-       System.out.println("// Export to GraphViz...");
-       var outputDir = "exports";
-       var path = Paths.get(outputDir, name + ".dot").toString();
-       new DotExporter(res,path,name).export();
-
-       System.out.println("// done");
+      System.out.println("// done");
 
       System.exit(0);
   }
 
-  
-    // public static List<List<BEvent>> getAllPaths(GenerateAllTracesInspection.MapperResult res) {
-    //   System.out.println("// Generated paths:");
-    //   boolean findSimplePathsOnly = true; // acyclic paths
-    //   int maxPathLength = Integer.MAX_VALUE;
-    //   var paths = res.generatePaths(findSimplePathsOnly, maxPathLength);
-    //   System.out.println(paths);
-    //   return paths;
-    // }
-  
+  private static void runBPModel() throws URISyntaxException {
+
+      Set<List<BEvent>> samples = new HashSet<>();
+
+      List<String> files =
+              Arrays.stream(Objects.requireNonNull(Path.of(Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource(example.name())).toURI()).toFile().listFiles()))
+                      .map(f -> String.join("/", example.name(), f.getName()))
+                      .collect(Collectors.toList());
+      System.out.println("files-"+files);
+      BProgram bprog = new ContextBProgram(files);
+      final BProgramRunner rnr = new BProgramRunner(bprog);
+      rnr.addListener(new PrintCOBProgramRunnerListener(logLevel, new PrintBProgramRunnerListener()));
+
+      if (example == Example.TicTacToe) {
+//        boolean useUI = true;
+//        TicTacToeGameMain.main(bprog, rnr, useUI);
+          return;
+      }
+      var eventLogger = rnr.addListener(new InMemoryEventLoggingListener());
+      rnr.run();
+      samples.add(eventLogger.getEvents());
+
+  }
+
+  static private void runMapper() throws Exception {
+      String name = "Bereshit";
+      BProgram bprog = new ContextBProgram(name + "/dal.js", name + "/bl.js");
+
+      StateSpaceMapper mpr = new StateSpaceMapper();
+      mpr.setMaxTraceLength(10);
+      var res = mpr.mapSpace(bprog);
+      System.out.println("// completed mapping the states graph");
+      System.out.println(res.toString());
+
+      System.out.println("// Export to GraphViz...");
+      var outputDir = "exports";
+      var path = Paths.get(outputDir, name + ".dot").toString();
+      new DotExporter(res,path,name).export();
+
+  }
 }
