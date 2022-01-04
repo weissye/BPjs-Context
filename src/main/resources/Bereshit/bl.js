4b02086083c6ca3361b6afc20f6176256973e4ad
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2022-01-04 11:01:19 +0200

    Achiya's update

--- src/main/resources/Bereshit/bl.js
+++ src/main/resources/Bereshit/bl.js
@@ -1,151 +1,161 @@
 //-----------------------------------------------
-bp.log.setLevel("Off");
+bp.log.setLevel('Off')
 
-  var AnyImuOn = bp.EventSet("AnyImuOn", e => e.name.startsWith("imuOn"))
-  var AnyStrOn = bp.EventSet("AnyStrOn", e => e.name.startsWith("strOn"))
-  var AnyThrusterOn = bp.EventSet("AnyThrusterOn", e => e.name.startsWith("thrusterOn"))
-  var AnyOpenUpstreamValves = bp.EventSet("AnyOpenUpstreamValvesAnyOpenUpstreamValves", e => e.name.startsWith("openUpstream"))
-  var AnyImuCalib = bp.EventSet("AnyImuCalib", e => e.name.startsWith("imuCalib"))
+const AnyStrOn = bp.EventSet('AnyStrOn', e => e.name.startsWith('strOn'))
+const AnyThrusterOn = bp.EventSet('AnyThrusterOn', e => e.name.startsWith('thrusterOn'))
+const AnyOpenUpstreamValves = bp.EventSet('AnyOpenUpstreamValvesAnyOpenUpstreamValves', e => e.name.startsWith('openUpstream'))
+const AnyImuCalib = bp.EventSet('AnyImuCalib', e => e.name.startsWith('imuCalib'))
 
-  ctx.bthread('Init', 'init', function (entity) {
+function hardwareEvent(nama, data, isStart) {
+  if (!data) data = {}
+  data.start = isStart
+  return Event(name, data)
+}
 
-      bp.log.info("Init "+entity.state)
+function hardwareAction(nane, data, cond) {
+  if (cond && cond())
+    return
+  sync({ request: hardwareEvent(name, data, true) })
+  sync({ waitFor: hardwareEvent(name, data, false) })
+}
 
-      //Check IMU, if "off", turn "on". if not calibrate, calibrate.
-      for ( imu in entity.imuStatus) {
-        bp.log.info("imu "+imu)
-        if (entity.imuStatus[imu].equals("off") )  // maybee doesn't requiered
-          sync({request: Event('imuOn', {no:imu})})
-      }
+ctx.bthread('Init', 'init', function (entity) {
+  bp.log.info('Init ' + entity.state)
 
-      for ( str in entity.strStatus) {
-        if (entity.strStatus[str].equals("off"))   // maybee doesn't requiered
-          sync({request: Event('strOn', {no:str})})
-      }
-      for ( thruster in entity.thrusterStatus) {
-        if (entity.thrusterStatus[thruster].equals("off"))   // maybee doesn't requiered
-          sync({request: Event('thrusterOn', {no:thruster})})
-        sync({request: Event('openUpstreamValves', {no:thruster})})
-      }
+  //Check IMU, if "off", turn "on". if not calibrate, calibrate.
+  for (imu in entity.imuStatus) {
+    bp.log.info('imu ' + imu)
+    hardwareAction('imuOn', { no: imu }, () => entity.imuStatus[imu].equals('on'))
+  }
 
-      sync({request: Event('powerOnLulav')})
-      sync({request: Event('powerOnRFLandingSensor')})
+  for (str in entity.strStatus) {
+    if (entity.strStatus[str].equals('off'))   // maybee doesn't required
+      sync({ request: Event('strOn', { no: str }) })
+  }
+  for (thruster in entity.thrusterStatus) {
+    if (entity.thrusterStatus[thruster].equals('off'))   // maybee doesn't required
+      sync({ request: Event('thrusterOn', { no: thruster }) })
+    sync({ request: Event('openUpstreamValves', { no: thruster }) })
+  }
 
-      controlMonitor = function(device, landingLimit)
-      {
-         bthread( 'Control monitor ' + device + ' ' + landingLimit , function()
-         {
-              while (true){
-                //Do Monitoring of 'device'
-                sync({waitFor: bp.all})
-              }
-        })
-      }
-      controlMonitor('termal', entity.landinLimitSec);
-      controlMonitor('baterryCharge', entity.landinLimitSec);
-      controlMonitor('batteryDOD', entity.landinLimitSec);
+  sync({ request: Event('powerOnLulav') })
+  sync({ request: Event('powerOnRFLandingSensor') })
 
-      if (entity.initCommStatus.equals('off') || entity.initCommStatus.equals('initCommEmer'))
-        sync({request: Event('activeInitComm')})
-
-      sync({request: Event('doBitImsAndPld')})
-
-      e = sync({request: Event('initEnded')})
-
-  })
-
-  bthread('imu Turn On', function () {
-    while(true){
-      e = sync({waitFor: AnyImuOn})
-        bp.log.info("in imuOn "+e.data.no)
-        //Do turn on imu[x]
-      sync({request: Event('imuTurnedOn',{no:e.data.no})})
-
-    }
-  })
-
-  ctx.bthread('doImuCalib', 'imu Calib req', function (entity) {
-    while(true){
-      //Do imu[x] calibration
-      //reset calibration timeoute for imu[x]
-      sync({request:Event('imuCalibrated', {no:entity.imuCurrentNo})})
-    }
-  })
-
-  bthread('str Turn On', function (entity) {
-    while(true){
-      e = sync({waitFor: AnyStrOn})
-      //Do turn on str[x]
-      sync({request: Event('strIsOn', {no:e.data.no})})
-    }
-  })
-
-  bthread('thruster Turn On', function () {
-    while(true){
-      e = sync({waitFor: AnyThrusterOn})
-      //Do turn on thruster[x]
-      sync({request: Event("thrusterIsOn", {no:e.data.no})})
-    }
-
-  })
-
-  bthread('open upstream thruster', function (entity) {
-    while(true){
-      e = sync({waitFor: [Event('openUpstreamValves')]})
-      entity.thrusterStatus[e.data.no] = "on"
-      //Do open upstream valves for thruster[x]
-    }
-  })
-
-    bthread('power on lulav', function () {
-      while(true){
-        e = sync({waitFor: [Event('powerOnLulav')]})
-        //Do power on lulav landing sesnsor
-        sync({request: Event("lulaStatusOn")})
+  let controlMonitor = function (device, landingLimit) {
+    bthread('Control monitor ' + device + ' ' + landingLimit, function () {
+      while (true) {
+        //Do Monitoring of 'device'
+        sync({ waitFor: bp.all })
       }
     })
-    bthread('power on RF landing sensor', function () {
-      while(true){
-        e = sync({waitFor: [Event('powerOnRFLandingSensor')]})
-        //Do power on RF landing sesnsor
-        sync({request: Event("RFLandingSensorOn")})
-      }
-    })
-    bthread('Active init comm', function () {
-      while(true){
-        e = sync({waitFor: [Event('activeInitComm')]})
-        //initiate communication system
-        sync({request: Event('commInit')})
-      }
-    })
+  }
+  controlMonitor('termal', entity.landinLimitSec)
+  controlMonitor('baterryCharge', entity.landinLimitSec)
+  controlMonitor('batteryDOD', entity.landinLimitSec)
 
-    bthread('Do bit Ims and Payload', function () {
-      while(true){
-        e = sync({waitFor: [Event('doBitImsAndPld')]})
-        doBit = function(device)
-        {
-           bthread( 'Bit ' + device , function()
-           {
-                  //Do Bit of 'device'
-                  e = sync({request: Event('powerOn'+device)})
-                  e = sync({request: Event('operate'+device)})
-                  e = sync({request: Event('powerOff'+device)})
-          })
-        }
-        doBit('Ims');  //Do bit to IMS
-        doBit('Payload');   //Do bit to Payload
-      }
-    })
-    ctx.bthread('End landing state', 'init ended', function()
-    {
-       while (true){
-           sync({waitFor: Event('initEnded')})
-           if (use_accepting_states) {
-             // AcceptingState.Continuing()
-             AcceptingState.Stopping()
-           }
-       }
-    })
+  if (entity.initCommStatus.equals('off') || entity.initCommStatus.equals('initCommEmer'))
+    sync({ request: Event('activeInitComm') })
+
+  sync({ request: Event('doBitImsAndPld') })
+
+
+  // till here - calibration must be finished
+  //gnc
+
+  sync({ request: Event('initEnded') })
+
+})
+
+ctx.bthread('imu Turn On', 'imu', function (imuid) {
+  while (true) {
+    sync({ waitFor: hardwareEvent('imuOn', { no: imuid }, true) })
+    bp.log.info('in imuOn ' + imuid)
+    // completed turn on imu[x] (triggers a ctx effect)
+    sync({ request: hardwareEvent('imuOn', { no: imuid }, false) })
+
+    //TODO check if calibration is initiated. if not - initiate it
+  }
+})
+
+ctx.bthread('doImuCalib', 'imu Calib req', function (entity) {
+  while (true) {
+    //Do imu[x] calibration
+    //reset calibration timeoute for imu[x]
+    sync({ request: Event('imuCalibrated', { no: entity.imuCurrentNo }) })
+  }
+})
+
+bthread('str Turn On', function (entity) {
+  while (true) {
+    e = sync({ waitFor: AnyStrOn })
+    //Do turn on str[x]
+    sync({ request: Event('strIsOn', { no: e.data.no }) })
+  }
+})
+
+bthread('thruster Turn On', function () {
+  while (true) {
+    e = sync({ waitFor: AnyThrusterOn })
+    //Do turn on thruster[x]
+    sync({ request: Event('thrusterIsOn', { no: e.data.no }) })
+  }
+
+})
+
+bthread('open upstream thruster', function (entity) {
+  while (true) {
+    e = sync({ waitFor: [Event('openUpstreamValves')] })
+    entity.thrusterStatus[e.data.no] = 'on'
+    //Do open upstream valves for thruster[x]
+  }
+})
+
+bthread('power on lulav', function () {
+  while (true) {
+    e = sync({ waitFor: [Event('powerOnLulav')] })
+    //Do power on lulav landing sesnsor
+    sync({ request: Event('lulaStatusOn') })
+  }
+})
+bthread('power on RF landing sensor', function () {
+  while (true) {
+    e = sync({ waitFor: [Event('powerOnRFLandingSensor')] })
+    //Do power on RF landing sesnsor
+    sync({ request: Event('RFLandingSensorOn') })
+  }
+})
+bthread('Active init comm', function () {
+  while (true) {
+    e = sync({ waitFor: [Event('activeInitComm')] })
+    //initiate communication system
+    sync({ request: Event('commInit') })
+  }
+})
+
+bthread('Do bit Ims and Payload', function () {
+  while (true) {
+    e = sync({ waitFor: [Event('doBitImsAndPld')] })
+    doBit = function (device) {
+      bthread('Bit ' + device, function () {
+        //Do Bit of 'device'
+        e = sync({ request: Event('powerOn' + device) })
+        e = sync({ request: Event('operate' + device) })
+        e = sync({ request: Event('powerOff' + device) })
+      })
+    }
+    doBit('Ims')  //Do bit to IMS
+    doBit('Payload')   //Do bit to Payload
+  }
+})
+ctx.bthread('End landing state', 'init ended', function () {
+  while (true) {
+    sync({ waitFor: Event('initEnded') })
+    if (use_accepting_states) {
+      // AcceptingState.Continuing()
+      AcceptingState.Stopping()
+    }
+  }
+})
 
 
 
--- src/main/resources/Bereshit/dal.js
+++ src/main/resources/Bereshit/dal.js
@@ -1,109 +1,120 @@
-  //----------------------------------------
-  ctx.populateContext([
-    ctx.Entity("bereshitLander", "bereshit", {
-        state: "Landing",
-        prevState: "PostSeperation",
-        imuStatus: ["off","off","off"],
-        thrusterStatus: ["off","off"],
-        strStatus: ["off","off"],
-        imuCalibFlag: ["no","no","no"],
-        valveStatus: ["open","open","close"],
-        lulavStatus: "on",
-        rfLandingSensorStatus: "on",
-        landinLimitSec: 100,
-       initCommStatus: "initCommEmer",
-        imsStatus: "off",
-        payloadStatus: "off",
-        subState: "init",
-        prevSunState: "none",
-        requieredInertial3Axis: [1,2,3],
-        requieredInertial3AxisTolerance:[0.1, 0.1, 0.1],
-        PONR: false,
-        initState: true,
-        imuCurrentNo: 0,
-    })
-  ])
-
-  ctx.registerQuery('init', function (entity) {
-    return entity.state  == "Landing" && entity.initState
+//----------------------------------------
+ctx.populateContext([
+  ctx.Entity('beresheetLander', 'beresheet', {
+    state: 'Landing',
+    prevState: 'PostSeparation',
+    // imuStatus: ['off', 'off', 'off'],
+    thrusterStatus: ['off', 'off'],
+    strStatus: ['off', 'off'],
+    // imuCalibFlag: ['no', 'no', 'no'],
+    valveStatus: ['open', 'open', 'close'],
+    lulavStatus: 'on',
+    rfLandingSensorStatus: 'on',
+    landinLimitSec: 100,
+    initCommStatus: 'initCommEmer',
+    imsStatus: 'off',
+    payloadStatus: 'off',
+    subState: 'init',
+    prevSunState: 'none',
+    requiredInertial3Axis: { x: 1, y: 2, z: 3 },
+    requiredInertial3AxisTolerance: { x: 0.1, y: 0.1, z: 0.1 },
+    PONR: false,
+    initState: true,
+    imuCurrentNo: 0
   })
+])
 
-  ctx.registerQuery('imu Calib req', function (entity) {
-    return entity.state  == "Landing" && entity.initState && entity.imuStatus[entity.imuCurrentNo].equals("on") && entity.imuCalibFlag[entity.imuCurrentNo].equals("no")
-  })
+for (let i = 0; i < 3; i++) {
+  ctx.populateContext(ctx.Entity('imu' + i, 'imu', { id: i, isOn: false, isCalibrated: false }))
+}
 
-  ctx.registerQuery('init ended', function (entity) {
-    return entity.state  == "Landing" && !entity.initState //&& entity.subState.equals("init")
-  })
+ctx.registerQuery('imu', function (entity) {
+  return entity.type == 'imu'
+})
 
-  ctx.registerQuery('oriantation', function (entity) {
-    return entity.state.equals("Landing") && GNCcheckOrientation()
-  })
+ctx.registerQuery('init', function (entity) {
+  return entity.type == 'beresheet' && entity.state == 'Landing' && entity.initState
+})
+
+ctx.registerQuery('uncalibrated imu', function (entity) {
+  return entity.type == 'imu' && !entity.isCalibrated == 'Landing'
+})
+
+ctx.registerQuery('init ended', function (entity) {
+  return entity.type == 'beresheet' && entity.state == 'Landing' && !entity.initState //&& entity.subState.equals("init")
+})
+
+ctx.registerQuery('oriantation', function (entity) {
+  return entity.state.equals('Landing') && GNCcheckOrientation()
+})
 
 
- ctx.registerEffect('initEnded', function () {
-    e = ctx.getEntityById('bereshitLander')
-    e.initState = false
-    ctx.updateEntity(e)
-  })
+ctx.registerEffect('initEnded', function (data) {
+  let e = ctx.getEntityById('beresheetLander')
+  e.initState = false
+  ctx.updateEntity(e)
+})
 
-  ctx.registerEffect('imuTurnedOn', function (data) {
-    bp.log.info("imuIsOn "+data.no)
-    e = ctx.getEntityById('bereshitLander')
-    e.imuStatus[data.no] = "on"
+ctx.registerEffect('imuOn', function (data) {
+  if (!data.started) {
+    bp.log.info('imuIsOn ' + data.no)
+    let e = ctx.getEntityById('beresheetLander')
+    e.imuStatus[data.no] = 'on'
     e.imuCurrentNo = data.no                    //save current imu as context
     ctx.updateEntity(e)
-  })
+  }
 
-  ctx.registerEffect('imuCalibrated', function (data) {
-    bp.log.info("imuCalibrated "+data.no)
-    e = ctx.getEntityById('bereshitLander')
-    e.imuCalibFlag[data.no] = "yes"
-    ctx.updateEntity(e)
-  })
+})
 
-  ctx.registerEffect('strIsOn', function (data) {
-    bp.log.info("strIsOn "+data.no)
-    e = ctx.getEntityById('bereshitLander')
-    e.strStatus[data.no] = "on"
-    ctx.updateEntity(e)
-  })
+ctx.registerEffect('imuCalibrated', function (data) {
+  bp.log.info('imuCalibrated ' + data.no)
+  let e = ctx.getEntityById('beresheetLander')
+  e.imuCalibFlag[data.no] = 'yes'
+  ctx.updateEntity(e)
+})
 
-  ctx.registerEffect('lulaStatusOn', function () {
-    bp.log.info("lulaStatusOn")
-    e = ctx.getEntityById('bereshitLander')
-    e.lulavStatus = "on"
-    ctx.updateEntity(e)
-  })
-  ctx.registerEffect('RFLandingSensorOn', function () {
-    bp.log.info("RFLandingSensorOn")
-    e = ctx.getEntityById('bereshitLander')
-    e.rfLandingSensorStatus = "on"
-    ctx.updateEntity(e)
-  })
-  ctx.registerEffect('commInit', function () {
-    bp.log.info("commInit")
-    e = ctx.getEntityById('bereshitLander')
-    e.initCommStatus = "Init_Comm"
-    ctx.updateEntity(e)
-  })
+ctx.registerEffect('strIsOn', function (data) {
+  bp.log.info('strIsOn ' + data.no)
+  let e = ctx.getEntityById('beresheetLander')
+  e.strStatus[data.no] = 'on'
+  ctx.updateEntity(e)
+})
 
-  ctx.registerEffect('thrusterIsOn', function (data) {
-    bp.log.info("thrusterIsOn "+data.no)
-    e = ctx.getEntityById('bereshitLander')
-      e.thrusterStatus[data.no] = "on"
-    ctx.updateEntity(e)
-  })
+ctx.registerEffect('lulaStatusOn', function (data) {
+  bp.log.info('lulaStatusOn')
+  let e = ctx.getEntityById('beresheetLander')
+  e.lulavStatus = 'on'
+  ctx.updateEntity(e)
+})
+ctx.registerEffect('RFLandingSensorOn', function (data) {
+  bp.log.info('RFLandingSensorOn')
+  let e = ctx.getEntityById('beresheetLander')
+  e.rfLandingSensorStatus = 'on'
+  ctx.updateEntity(e)
+})
+ctx.registerEffect('commInit', function (data) {
+  bp.log.info('commInit')
+  let e = ctx.getEntityById('beresheetLander')
+  e.initCommStatus = 'Init_Comm'
+  ctx.updateEntity(e)
+})
 
- ctx.registerEffect('orientation', function () {
-    e = ctx.getEntityById('bereshitLander')
-    e.initState = false
-    ctx.updateEntity(e)
-  })
+ctx.registerEffect('thrusterIsOn', function (data) {
+  bp.log.info('thrusterIsOn ' + data.no)
+  let e = ctx.getEntityById('beresheetLander')
+  e.thrusterStatus[data.no] = 'on'
+  ctx.updateEntity(e)
+})
+
+ctx.registerEffect('orientation', function (data) {
+  let e = ctx.getEntityById('beresheetLander')
+  e.initState = false
+  ctx.updateEntity(e)
+})
 
 function GNCcheckOrientation() {
-  e = ctx.getEntityById('bereshitLander')
+  let e = ctx.getEntityById('beresheetLander')
   if (true)
 
-  return true;
+    return true
 }
